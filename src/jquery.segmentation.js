226fe11efba89f70929b4637768239f1796ac1a8
Libor Jonát <libor@jonat.cz>
2013-02-09 18:21:00 +0100

    New features: Segmentation X and Y, reverse

--- src/jquery.segmentation.js
+++ src/jquery.segmentation.js
@@ -1,34 +1,59 @@
 (function( $ ){
-    $.fn.segment = function(count, timeout, animate){
+    $.fn.segment = function(params, timeout, animate){
+        var countX = 1;
+        var countY = 1;
+        var reverse = false;
+        if( typeof params === 'number' ){
+          countX = params;
+        }else if ( Array.isArray(params) ){
+          countX = params[0];
+          countY = params[1];
+        }else if( typeof params === 'object' ){
+          if(params['x'] != undefined) countX = params['x'];
+          if(params['y'] != undefined) countY = params['y'];
+          if(params['reverse'] != undefined) reverse = params['reverse'];
+          //if(params[''] != undefined) = params['']; 
+        }
         var height = $(this).height();
         var width  = $(this).width();
-        var segmentWidth = width / count;
+
+        var segmentWidth = width / countX;
+        var segmentHeight = height / countY;
         
         var elements = [];
-        for(i = 0; i < count; i++){
+        for(j = 0; j < countY; j++){
+          for(i = 0; i < countX; i++){  
             var e = document.createElement('div');
             $(e).css('z-index', '100');
             $(e).css('float', 'left');
             $(e).css('background', $(this).css('background'));
-            $(e).css('height', height + 'px');
-            $(e).css('width', segmentWidth + 'px');
+            
+            var left = (width - i * segmentWidth);
+            var top = (height - j * segmentHeight);
+            
+            $(e).css('background-position', left + 'px ' + top + 'px');
+            $(e).css('height', segmentHeight + 'px');
+            $(e).css('width',  segmentWidth  + 'px');
+            
+            var position = {
+                top: $(this).position().top + j * segmentHeight,
+                left: $(this).position().left + i * segmentWidth
+            };
+            $(e).css('position', 'absolute');
+            $(e).css(position);
+            
             $(e).addClass('segment');
             $(e).appendTo( $(this) );
+           
             elements.push(e);
+          }
         }
         
         $(this).css('overflow', 'show');
         $(this).css('background', 'none');
         
-        $.each(elements, function(k, v){
-            var position = {
-                top: $(this).position().top,
-                left: $(this).position().left + k * segmentWidth
-            };
-            
-            $(v).css('position', 'absolute');
-            $(v).css(position);
-            
+        
+        $.each( ( (!reverse) ? elements : elements.reverse() ) , function(k, v){
             setTimeout(function(){
                 animate(v);
             }, timeout * k);
