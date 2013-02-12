6b676371fd88c05026cb532ec65eb720438461b0
Libor Jonát <jonatlib@fit.cvut.cz>
2013-02-12 17:23:27 +0100

    New demo, all parameters inject by object, added itterator and array setter

--- src/jquery.segmentation.js
+++ src/jquery.segmentation.js
@@ -1,33 +1,27 @@
 (function( $ ){
-    $.fn.segment = function(params, timeout, animate){
+    
+    function getParams(params, value, def){
+        if(params[value] == undefined) return def;
+        return params[value];
+    }
+    
+    $.fn.segment = function(params){
         if($(this).length > 1){
-          var result = [];
-          $.each($(this), function(k, v){
-            result.push( $(v).segment(params, timeout, animate) );
-          });
-          return result;
-        }
-        
-        var countX = 1;
-        var countY = 1;
-        var reverse = false;
-        if( typeof params === 'number' ){
-          countX = params;
-        }else if ( Array.isArray(params) ){
-          countX = params[0];
-          countY = params[1];
-        }else if( typeof params === 'object' ){
-          if(params['x'] != undefined) countX = params['x'];
-          if(params['y'] != undefined) countY = params['y'];
-          if(params['reverse'] != undefined) reverse = params['reverse'];
-          //if(params[''] != undefined) = params['']; 
+            var result = [];
+            $.each($(this), function(k, v){
+                result.push( $(v).segment(params, timeout, animate) );
+            });
+            return result;
         }
         
         var height = $(this).outerHeight();
         var width  = $(this).outerWidth();
         
+        var countX = getParams(params, 'x', width / getParams(params, 'size', 100));
+        var countY = getParams(params, 'y', height / getParams(params, 'size', 100));
+        var reverse = getParams(params, 'reverse', false);
+        
         var sourcePosition = $(this).position();
-        var sourceBackground = $(this).css('background'); 
 
         var segmentWidth  = parseInt(width / countX);
         var segmentHeight = parseInt(height / countY);
@@ -40,47 +34,73 @@
         
         var elements = [];
         for(j = 0; j < countY; j++){
-          for(i = 0; i < countX; i++){  
-            var e = document.createElement('div');
-            $(e).css('z-index', '100');
-            $(e).css('float', 'left');
-            $(e).css('overflow', 'hidden');
-            $(e).css('height', segmentHeight + 'px');
-            $(e).css('width',  segmentWidth  + 'px');
-                        
-            //var left = (width - i * segmentWidth);
-            //var top = (height - j * segmentHeight);
-            //$(e).css('background', sourceBackground);
-            //$(e).css('background-position', left + 'px ' + top + 'px');
+            for(i = 0; i < countX; i++){  
+                var e = document.createElement('div');
+                $(e).css('z-index', '100');
+                $(e).css('float', 'left');
+                $(e).css('overflow', 'hidden');
+                $(e).css('height', segmentHeight + 'px');
+                $(e).css('width',  segmentWidth  + 'px');
             
-            $(element).clone().css({
-              marginLeft: - i * segmentWidth,
-              marginTop : - j * segmentHeight
-            }).appendTo(e);
+                $(element).clone().css({
+                    marginLeft: - i * segmentWidth,
+                    marginTop : - j * segmentHeight
+                }).appendTo(e);
             
-            var position = {
-                top: sourcePosition.top + j * segmentHeight,
-                left: sourcePosition.left + i * segmentWidth
-            };
-            $(e).css('position', 'absolute');
-            $(e).css(position);
+                var position = {
+                    top: sourcePosition.top + j * segmentHeight,
+                    left: sourcePosition.left + i * segmentWidth
+                };
+                $(e).css('position', 'absolute');
+                $(e).css(position);
             
-            $(e).addClass('segment');
-            $(e).appendTo( $(this) );
+                $(e).addClass('segment');
+                $(e).appendTo( $(this) );
            
-            elements.push(e);
-          }
+                elements.push(e);
+            }
         }        
         
         var removeElement = $(this);
         var remove = function(){
-          removeElement.remove();
+            removeElement.remove();
         };
-        $.each( ( (!reverse) ? elements : elements.reverse() ) , function(k, v){
+        
+        var getter = getParams(params, 'getter', function(elements){
+            if(reverse){
+                return elements.reverse();
+            }else{
+                return elements;
+            }
+        });
+        
+        var itterator = getParams(params, 'itterator', function(elements, index){
+            return elements[index];
+        });
+        
+        var animate = getParams(params, 'animate', function( el ){
+            $(el).fadeOut(getParams(params, 'duration', 800), function(){
+                $(el).remove();
+                if(index >= length - 1) remove();
+            });
+        });
+        var timeout = getParams(params, 'timeout', 50);
+        
+        var el = getter(elements);
+        $.each(el , function(k, v){
             setTimeout(function(){
-                animate(v, k, elements.length, remove);
+                animate(itterator(el, k), k, elements.length, remove);
             }, timeout * k);
         });
+        
+        var done = getParams(params, 'done', function(r){
+            r();
+        });
+        
+        setTimeout(function(){
+            done(remove);
+        }, timeout * elements.length + getParams(params, 'duration', 800));
+        
         return this;
     };
 })( jQuery );
